9195c2e7ee577d57102dba39d2ac187a2d0ae469
M13K <ag.koffi@hotmail.fr>
2017-02-28 18:14:05 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$students= array("Emmanuel"=>"42","Thierry"=>"51","Pascal"=>"45","Eric"=>"48","Nicolas"=>"19");
+
+print_r($students);
+
+
+
+    $moy = array_sum($students)/count($students);
+
+    echo "la moyenne d'âge est de " .$moy."ans";
+
+
+
+
